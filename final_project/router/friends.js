0c93bc1e46323c60c010f236f0c53be939669af3
Alex Khodyka <110336908+AlexKhodyka@users.noreply.github.com>
2023-10-23 18:41:09 +0300

    Create friends.js

---
+++ final_project/router/friends.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const router = express.Router();
+
+let friends = {
+    "johnsmith@gamil.com": {"firstName": "John","lastName": "Doe","DOB":"22-12-1990"},
+    "annasmith@gamil.com":{"firstName": "Anna","lastName": "smith","DOB":"02-07-1983"},
+    "peterjones@gamil.com":{"firstName": "Peter","lastName": "Jones","DOB":"21-03-1989"}
+};
+
+
+router.get("/",(req,res)=>{
+    res.send(JSON.stringify(friends,null,4));
+
+});
+
+router.get('/:email',function (req, res) {
+const email = req.params.email;
+res.send(friends[email])
+});
+
+
+router.post("/",function (req,res){
+    if (req.body.email){
+        friends[req.body.email] = {
+            "firstName":req.body.firstName,
+            //Add similarly for lastName
+            //Add similarly for DOB
+            }
+    }
+res.send("The user" + (' ')+ (req.body.firstName) + " Has been added!");
+});
+
+
+router.put("/:email", function (req, res) {
+    const email = req.params.email;
+    let friend = friends[email]
+    if (friend) { //Check is friend exists
+        let DOB = req.body.DOB;
+        //Add similarly for firstName
+        //Add similarly for lastName
+
+        //if DOB the DOB has been changed, update the DOB 
+        if(DOB) {
+            friend["DOB"] = DOB
+        }
+        //Add similarly for firstName
+        //Add similarly for lastName
+        friends[email]=friend;
+        res.send(`Friend with the email  ${email} updated.`);
+    }
+    else{
+        res.send("Unable to find friend!");
+    }
+  });
+
+
+  router.delete("/:email", (req, res) => {
+    const email = req.params.email;
+    if (email){
+        delete friends[email]
+    }
+    res.send(`Friend with the email  ${email} deleted.`);
+  });
+
+module.exports=router;
